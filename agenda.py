7b17e3f7b14431512cbc886fcae4476cf2e7c8cb
Adrian Kevem <adrianveven@gmail.com>
2022-11-30 14:01:02 -0300

    update

--- agenda.py
+++ agenda.py
@@ -6,12 +6,86 @@ AGENDA["Guilherme"] = {
     "Endereço": "Rua Fulano de Tal, Nº 1"
 }
 
+AGENDA["Patrick"] = {
+    "Telefone": "(31) 99999-1111",
+    "Email": "patrick@provedor.com",
+    "Endereço": "Rua Fulano de Tal, Nº 2"
+}
+
 def mostrar_contatos():
     for contato in AGENDA:
-        print("\nNome:" + contato)
-        print("Telefone: " + AGENDA[contato]["Telefone"])
-        print("Email: " + AGENDA[contato]["Email"])
-        print("Endereço: " + AGENDA[contato]["Endereço"] + "\n")
+        print("\nNome: " + contato)
+        for item in AGENDA[contato]:
+            print("Telefone: " + AGENDA[contato][item])
 
+def buscar_contato(contato_buscado):
+    encontrado = False
+    for contato in AGENDA:
+        if(contato == contato_buscado):
+            encontrado = True
+            print("\nNome: " + contato)
+            print("Telefone: " + AGENDA[contato]["Telefone"])
+            print("Email: " + AGENDA[contato]["Email"])
+            print("Endereço: " + AGENDA[contato]["Endereço"] + "\n")
+    if(encontrado == False):
+        print("\n>>>>>>>>>> Contato não encontrado!\n")
+
+def adicionar_contato():
+    nome = input("\nNome: ")
+    AGENDA[nome] = {
+        "Telefone": input("Telefone: "),
+        "Email": input("Email: "),
+        "Endereço": input("Endereço: "),
+    }
+    print("\n>>>>>>>>>> Contato adicionado!\n")
+
+def editar_contato(contato_buscado):
+    encontrado = False
+    for contato in AGENDA:
+        if(contato == contato_buscado):
+            encontrado = True
+            AGENDA[contato] = {
+                "Telefone": input("Telefone: "),
+                "Email": input("Email: "),
+                "Endereço": input("Endereço: "),
+            }
+            print("\n>>>>>>>>>> Contato editado!\n")
+    if(encontrado == False):
+        print("\n>>>>>>>>>> Contato não encontrado!\n")
+    
+def excluir_contato(contato_buscado):
+    AGENDA.pop(contato_buscado)
+    print("\n>>>>>>>>>> Contato excluído!\n")
+
+print("\nTODOS OS CONTATOS")
 mostrar_contatos()
 
+print("\n-------------------------------------------------")
+
+print("\nBUSCAR CONTATOS")
+buscar_contato(input("\nContato buscado: "))
+
+print("\n-------------------------------------------------")
+
+print("\nINCLUIR CONTATO")
+adicionar_contato()
+
+print("\n-------------------------------------------------")
+
+print("\nEDITAR CONTATO")
+editar_contato(input("\nContato a ser editado: "))
+
+print("\n-------------------------------------------------")
+
+print("\nTODOS OS CONTATOS")
+mostrar_contatos()
+
+print("\n-------------------------------------------------")
+
+print("\nEXCLUIR CONTATO")
+excluir_contato(input("\nContato a ser excluído: "))
+
+print("\nTODOS OS CONTATOS")
+mostrar_contatos()
+
+print("\n-------------------------------------------------")
--- teste.py
+++ teste.py
@@ -1,5 +1,2 @@
-lista_cores = ["azul", "verde", "amarelo", "preto"]
-lista_cores.append("vermelho")
-lista_cores[0] = "branco"
-tupla_cores = {"azul", "verde", "amarelo", "preto"}
-tupla_cores[0] = "branco"
+idade = None
+print(idade)
